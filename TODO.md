154239ee4a49978ec24fbdbf65cbc0d248e59ce2
Ruben Oosterink <jb.ict.info@gmail.com>
2023-07-25 10:43:17 +0200

    Maak het formulier werkend

--- TODO.md
+++ TODO.md
@@ -1,7 +1,10 @@
-- env file maken
-- SEO optimaliseren
-- gh-pages
 - file input andere styling
-- email js
-- validaties
 - links header fixen
+- SEO optimaliseren
+- svg px ipv %
+- footer/header full width
+- div id root: display contents
+- Footer kleiner maker en responsive
+- feedback emma
+
+- voor livegang de /defrissepitch/ prefix weghalen
--- src/components/Inschrijven/Inschrijven.tsx
+++ src/components/Inschrijven/Inschrijven.tsx
@@ -1,10 +1,67 @@
 import "./styles.scss";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Inschrijven: React.FunctionComponent = () => {
+  const [succes, setSucces] = useState(false);
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    //handle validation
+    if (!event.currentTarget.checkValidity()) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    event.currentTarget.className = "was-validated";
+
+    if (event.currentTarget.checkValidity()) {
+      const formData = new FormData(event.currentTarget);
+      // When checking the file input reference, it's good practice to check if current and files exist:
+      fetch("https://usebasin.com/f/ba72d7ff7419", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => {
+          if (response.ok) {
+            setSucces(true);
+            toast.success("Je pitch is succesvol verstuurd!");
+          }
+          if (!response.ok)
+            toast.error(
+              "Oeps er is iets fout gegaan, neem contact op met wijzijn@stadslabzeist.nl!",
+            );
+          return response.json();
+        })
+        .then((data) => console.log(data))
+        .catch((error) => {
+          toast.error(
+            "Oeps er is iets fout gegaan, neem contact op met wijzijn@stadslabzeist.nl!",
+          );
+          console.log(error);
+        });
+    }
+  };
   return (
     <section className="text-start">
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
       <h1 className="inschrijven-titel">inschrijven</h1>
-      <form>
+      <form
+        onSubmit={handleSubmit}
+        encType="multipart/form-data"
+        className="needs-validation"
+        noValidate
+      >
         <img
           className="svg-oog"
           src="/defrissepitch/assets/svg/FP_sticker_RGB_oog.svg"
@@ -20,7 +77,7 @@ const Inschrijven: React.FunctionComponent = () => {
           src="/defrissepitch/assets/svg/FP_sticker_RGB_recycle.svg"
           alt="Sticker recycle"
         />
-        <fieldset>
+        <fieldset disabled={succes}>
           <legend>Contactgegevens</legend>
           <div className="mb-lg-3 mb-1 row">
             <label htmlFor="inputIdee" className="col-sm-3 col-form-label">
@@ -31,10 +88,14 @@ const Inschrijven: React.FunctionComponent = () => {
                 type="text"
                 id="inputIdee"
                 required
+                name="Naam idee"
                 className="form-control"
                 placeholder="Mijn goede idee!"
                 aria-describedby="ideeHelp"
               />
+              <div className="invalid-feedback">
+                Geef een naam voor het idee op.
+              </div>
             </div>
           </div>
 
@@ -50,10 +111,14 @@ const Inschrijven: React.FunctionComponent = () => {
                 type="text"
                 id="inputContactpersoon"
                 required
+                name="Contactpersoon"
                 className="form-control"
                 placeholder="Voornaam Achternaam"
                 aria-describedby="contactpersoonHelp"
               />
+              <div className="invalid-feedback">
+                Geef een contactpersoon op.
+              </div>
             </div>
           </div>
 
@@ -66,20 +131,26 @@ const Inschrijven: React.FunctionComponent = () => {
                     type="text"
                     id="inputPostcode"
                     required
+                    name="Postcode"
                     className="form-control mb-2"
                     placeholder="Postcode"
                     aria-describedby="postcodeHelp"
                   />
+                  <div className="invalid-feedback">Geef een postcode op.</div>
                 </div>
                 <div className="col-6 col-sm-3">
                   <input
                     type="text"
                     id="inputHuisNummer"
                     required
+                    name="HuisNummer"
                     className="form-control mb-2"
                     placeholder="Huisnr en toevoeging"
                     aria-describedby="huisNummerHelp"
                   />
+                  <div className="invalid-feedback">
+                    Geef een huisnummer op.
+                  </div>
                 </div>
               </div>
               <div className="row">
@@ -88,20 +159,26 @@ const Inschrijven: React.FunctionComponent = () => {
                     type="text"
                     id="inputPlaats"
                     required
+                    name="Plaats"
                     className="form-control mb-2"
                     placeholder="Plaats"
                     aria-describedby="plaatsHelp"
                   />
+                  <div className="invalid-feedback">Geef een plaats op.</div>
                 </div>
                 <div className="col-12 col-sm-6">
                   <input
                     type="text"
                     id="inputStraatnaam"
+                    name="Straatnaam"
                     required
                     className="form-control mb-2"
                     placeholder="Straatnaam"
                     aria-describedby="straatnaamHelp"
                   />
+                  <div className="invalid-feedback">
+                    Geef een straatnaam op.
+                  </div>
                 </div>
               </div>
             </div>
@@ -119,10 +196,15 @@ const Inschrijven: React.FunctionComponent = () => {
                 type="tel"
                 id="inputTelefoonnummer"
                 required
+                name="Telefoonnummer"
                 className="form-control"
                 placeholder="06 - 1245678"
+                pattern="^\(?([+]31|0031|0)-?6(\s?|-)([0-9]\s{0,3}){8}$"
                 aria-describedby="telefoonnummerHelp"
               />
+              <div className="invalid-feedback">
+                Geef een valide telefoonnummer op.
+              </div>
             </div>
           </div>
 
@@ -137,11 +219,15 @@ const Inschrijven: React.FunctionComponent = () => {
               <input
                 type="email"
                 id="inputEmailadres"
+                name="Emailadres"
                 required
                 className="form-control"
                 placeholder="email@adres.nl"
                 aria-describedby="emailHelp"
-              />
+              />{" "}
+              <div className="invalid-feedback">
+                Geef een valide emailadres op.
+              </div>
             </div>
           </div>
 
@@ -153,14 +239,18 @@ const Inschrijven: React.FunctionComponent = () => {
               <input
                 type="url"
                 id="inputWebsite"
+                name="Website"
                 className="form-control"
-                placeholder="www.website.nl"
+                placeholder="https://www.website.nl"
                 aria-describedby="websiteHelp"
               />
+              <div className="invalid-feedback">
+                Geef een valide website op.
+              </div>
             </div>
           </div>
         </fieldset>
-        <fieldset>
+        <fieldset disabled={succes}>
           <legend>Jouw frisse idee</legend>
           <div className="mb-lg-4 mb-2">
             <label htmlFor="input1" className="form-label">
@@ -174,6 +264,7 @@ const Inschrijven: React.FunctionComponent = () => {
               className="form-control mt-2 mt-lg-3"
               id="input1"
               rows={3}
+              name="1. Vertel ons over je idee"
               placeholder="Mijn idee..."
               aria-describedby="input1Help"
               aria-label="Vraag 1"
@@ -191,6 +282,7 @@ const Inschrijven: React.FunctionComponent = () => {
             <textarea
               className="form-control"
               id="input2"
+              name="2. Wat hoop je te bereiken?"
               rows={3}
               aria-describedby="input2Help"
               aria-label="Vraag 2"
@@ -205,6 +297,7 @@ const Inschrijven: React.FunctionComponent = () => {
             <textarea
               className="form-control"
               id="input3"
+              name="3. Vertel iets over jezelf en je mede-initiatiefnemers"
               rows={3}
               aria-describedby="input3Help"
               aria-label="Vraag 3"
@@ -221,6 +314,7 @@ const Inschrijven: React.FunctionComponent = () => {
               className="form-control"
               id="input4"
               rows={3}
+              name="4. Wie zijn je eventuele samenwerkings-partners en de taakverdeling"
               aria-describedby="input4Help"
               aria-label="Vraag 4"
               maxLength={300}
@@ -240,6 +334,7 @@ const Inschrijven: React.FunctionComponent = () => {
               className="form-control mt-2 mt-lg-3"
               id="input5"
               rows={3}
+              name="5. Geef een globaal overzicht van de planning voor de realisatie van je idee"
               aria-describedby="input5Help"
               aria-label="Vraag 5"
               maxLength={300}
@@ -254,6 +349,7 @@ const Inschrijven: React.FunctionComponent = () => {
               className="form-control"
               id="input6"
               rows={3}
+              name="6. Hoe match jouw idee met onze vijf criteria?"
               aria-describedby="input6Help"
               aria-label="Vraag 6"
               maxLength={300}
@@ -274,6 +370,7 @@ const Inschrijven: React.FunctionComponent = () => {
               className="form-control mt-2 mt-lg-3"
               id="input7"
               rows={3}
+              name="7. Geef kort aan wat de verwachte uitgaven zijn voor de belangrijkste kostenposten"
               aria-describedby="input7Help"
               aria-label="Vraag 7"
               maxLength={300}
@@ -293,6 +390,7 @@ const Inschrijven: React.FunctionComponent = () => {
               className="form-control mt-2 mt-lg-3"
               id="input8"
               rows={3}
+              name="8. Hoe ga je de 1000 Euro gebruiken?"
               aria-describedby="input8Help"
               aria-label="Vraag 8"
               maxLength={300}
@@ -308,7 +406,8 @@ const Inschrijven: React.FunctionComponent = () => {
               className="form-control mb-2"
               type="file"
               id="formFile"
-              accept="image/*,video/*"
+              name="fotos[]"
+              accept="image/*"
               aria-label="Bijlagen toevoegen"
               aria-describedby="formFileInstruction"
               multiple
@@ -318,8 +417,7 @@ const Inschrijven: React.FunctionComponent = () => {
             </small>
           </div>
         </fieldset>
-
-        <button type="submit" className="btn btn-primary">
+        <button type="submit" className="btn btn-primary" disabled={succes}>
           Inschrijven!
         </button>
       </form>
